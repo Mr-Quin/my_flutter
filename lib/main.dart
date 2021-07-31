f81be5e0ff6cbb8af8ed71ee56534f25e5cae5ae
Mr-Quin <hr82003@gmail.com>
2021-07-30 17:31:39 -0700

    added drawer

--- lib/main.dart
+++ lib/main.dart
@@ -5,106 +5,86 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(home: App()));
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Welcome to Flutter', home: RandomWords());
+    return Container(width: 50, child: SideBar());
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class SideBar extends StatelessWidget {
+  const SideBar({Key? key}) : super(key: key);
 
   @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = <WordPair>{};
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        // NEW lines from here...
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.only(left: 30, top: 20),
+        children: const <Widget>[
+          DrawerSection(
+              children: const <Widget>[ListTile(title: Text('My Info'))]),
+          DrawerSection(title: 'Library', children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.person_outline),
+              title: Text('My Imports'),
             ),
-            body: ListView(children: divided),
-          );
-        }, // ...to here.
-      ),
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  Widget _buildSuggestions() => ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider();
-        /*2*/
-
-        final index = i ~/ 2; /*3*/
-
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-
-        return _buildRow(_suggestions[index]);
-      });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-        actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+            ListTile(
+              leading: Icon(Icons.view_list),
+              title: Text('Mass Editor'),
+            ),
+          ]),
+          DrawerSection(title: 'Activity', children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.wb_cloudy_outlined),
+              title: Text('Queue'),
+            ),
+            ListTile(
+              leading: Icon(Icons.history),
+              title: Text('History'),
+            ),
+          ]),
+          DrawerSection(title: 'Setting', children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('General'),
+            ),
+            ListTile(
+              leading: Icon(Icons.perm_media),
+              title: Text('Media Management'),
+            ),
+          ]),
         ],
       ),
-      body: _buildSuggestions(),
     );
+  }
+}
+
+class DrawerSection extends StatelessWidget {
+  const DrawerSection({Key? key, this.title, required this.children})
+      : super(key: key);
+
+  final String? title;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(top: 20),
+        child: Column(
+          children: <Widget>[
+            // if title is not null, display a title tile
+            (title != null
+                ? ListTile(
+                    title: Text(title!),
+                    dense: true,
+                  )
+                : Container()),
+            ...children
+          ],
+        ));
   }
 }
